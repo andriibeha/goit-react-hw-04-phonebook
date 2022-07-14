72029c9d5b22e2bc8f7970a42b1323123e55dfbc
andriibeha <beha.andrii@gmail.com>
2022-07-14 10:15:36 +0300

    fix useState and UseEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,15 @@ import Filter from "../Filter/Filter";
 import s from './App.module.css';
 
 
-let visibleContacts = [];
-
 function App() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState(
+        () => JSON.parse(window.localStorage.getItem('contacts')) ?? [],
+    );
+
     const [filter, setFilter] = useState("");
 
-    //componentDidMount
     useEffect(() => {
-        const getContacts = localStorage.getItem('contacts');
-        const parsedContacts = JSON.parse(getContacts);
-
-        if (parsedContacts) { 
-            setContacts([...parsedContacts]);
-        };
-    }, []); 
-
-
-    //componentDidUpdate   ПРОБЛЕМА ТУТ !!!!
-    useEffect(() => {
-        if (contacts.length === 0) {
-            return
-        };
-        //ВОТ ДЕСЬ ТУТ!!!!
-        localStorage.setItem('contacts', JSON.stringify(contacts));
+        window.localStorage.setItem('contacts', JSON.stringify(contacts));
     }, [contacts]);
 
     const handleSubmit = data => {
@@ -57,6 +42,7 @@ function App() {
         );
     };
 
+    let visibleContacts = [];
     visibleContacts = getVisibleContacts();
 
 
@@ -84,87 +70,3 @@ function App() {
 export default App;
 
 
-////////////
-
-
-/* class OldApp extends Component { 
-    state = {
-        contacts: [],
-        filter: ""
-    };
-
-    componentDidMount() {
-        const contacts = localStorage.getItem('contacts');
-        const parsedContacts = JSON.parse(contacts);
-
-        if (parsedContacts) { 
-            this.setState({ contacts: parsedContacts })
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.contacts !== prevState.contacts) { 
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-        }
-    };
-
-    onSubmit = data => {
-        let findName = this.state.contacts.find(item => item.name === data.name)
-
-        if (findName) { 
-            return alert(`${data.name} is already in contact`)
-        } else { 
-            this.setState(({contacts}) => ({
-            contacts: [...contacts, data]
-            }))
-            
-        }
-    };
-
-    onFilter = (e) => {
-        this.setState({filter: e.target.value})
-    };
-
-    handleClickDeleteBtn = (id) => {
-        this.setState(prevState => ({
-            contacts: prevState.contacts.filter(
-                contact => contact.id !== id
-            )
-        }))
-    }
-
-    getVisibleContacts = () => {
-        const { contacts, filter } = this.state
-        const filterTolowerCase = filter.toLowerCase();
-
-        return contacts.filter(
-            contact => contact.name.toLowerCase().includes(filterTolowerCase) 
-        )
-    };
-
-    render() { 
-        const { filter } = this.state;
-        const visibleContacts = this.getVisibleContacts()
-
-        return (
-            <div className={s.container}>
-                <h1>Phonebook</h1>
-                <From
-                    onSubmit={this.onSubmit}
-                />
-
-                <h2>Contacts</h2>
-                <Filter
-                    value={filter}
-                    onChange={this.onFilter}
-                />
-
-                <ContactList
-                    contacts={visibleContacts}
-                    onDeleteClick={this.handleClickDeleteBtn}
-                />
-            </div>
-        ) 
-    }
-}
- */
